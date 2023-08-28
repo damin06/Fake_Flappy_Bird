804670d00c1a6df8686300ff0f871f47dde0006a
JangWheesung <gameplay1qaz@gmail.com>
2023-08-29 08:54:08 +0900

    PlayerInputAndJump

---
+++ Assets/03_Script/Player/PlayerInput.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public event Action OnJump;
+    public event Action OnDead;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJump?.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.transform.tag == "Ground" || obj.transform.tag == "Pipe")
+        {
+            OnDead?.Invoke();
+        }
+    }
+}
