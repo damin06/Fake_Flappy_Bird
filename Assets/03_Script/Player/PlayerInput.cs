0c3370d6b244b57393f6149c59fc2d3f8ea85c05
JangWheesung <gameplay1qaz@gmail.com>
2023-08-29 11:10:26 +0900

    PipeSpawner And Score

--- Assets/03_Script/Player/PlayerInput.cs
+++ Assets/03_Script/Player/PlayerInput.cs
@@ -8,6 +8,8 @@ public class PlayerInput : MonoBehaviour
     public event Action OnJump;
     public event Action OnDead;
 
+    bool isDead;
+
     private void Update()
     {
         EventInvoke();
@@ -23,9 +25,11 @@ public class PlayerInput : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D obj)
     {
-        if (obj.transform.tag == "Ground" || obj.transform.tag == "Pipe")
+        if (!isDead && (obj.transform.tag == "Ground" || obj.transform.tag == "Pipe"))
         {
+            isDead = true;
             OnDead?.Invoke();
+            Time.timeScale = 0;//데드이벤트 잘 넣으면 지울거임
         }
     }
 }
